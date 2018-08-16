12bf8bb1923420646153ac6c4f23f596f9fae5d7
atomicn50 <tinhle46@gmail.com>
2018-08-15 17:13:57 -0700

    Added currentVideo reducer function. Associated tests passed

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,16 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  var newState = {};
+
+  if (action.type === 'CHANGE_VIDEO') {
+    newState = action.video;
+  } else {
+    return state;
+  }
+  
+  return newState;
 };
 
 export default currentVideoReducer;
