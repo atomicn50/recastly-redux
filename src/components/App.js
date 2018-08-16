460f14e6927b8dd66cd85b013aff41740236ef40
atomicn50 <tinhle46@gmail.com>
2018-08-15 20:24:56 -0700

    In progress - replacing render components with containers

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchContainer from '../containers/SearchContainer.js';
 import VideoListContainer from '../containers/VideoListContainer.js';
 import VideoPlayerContainer from '../containers/VideoPlayerContainer.js';
 import Nav from './Nav.js';
@@ -46,16 +47,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
+        <SearchContainer />
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
