fa6cff9f3a073433d432ec7b2a95c5b083755df6
atomicn50 <tinhle46@gmail.com>
2018-08-15 19:52:20 -0700

    Created search container, began work on videoList container

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,20 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleSearchInputChange: q => {
+      dispatch(handleSearchChange(q))
+    }
+  }
+}
+
+const SearchContainer = connect (
+  null,
+  mapDispatchToProps
+  )(Search)
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
